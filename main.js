5a7c90d76bcfee78665ae5c4c3e9f4842dc0822b
Handemore <handemore7@gmail.com>
2022-05-24 01:51:48 -0500

    added changes in form

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-var sliderMoney = document.querySelector(".slider_money");
-var sliderTime = document.querySelector(".slider_time");
-var precisionpercen = document.querySelector(".textInputAverage");
+var fecha = document.querySelector('#fecha');
+var presupuesto = document.querySelector('#presupuesto');
+var tiempo = document.querySelector('#tiempo');
+var proyecto = document.querySelector('#proyecto');
+var descripcion = document.querySelector('#descripcion');
+var etapa = document.querySelector('#etapa');
+var metodologia = document.querySelector('#metodologia');
+var herramientas = document.querySelector('#herramientas');
 
-var initValMoney = 5;
-var initValTime = 8;
 
-sliderMoney.value = initValMoney;
-sliderTime.value = initValTime;
 
-sliderMoney.addEventListener('change', ()=>{document.querySelector('.textInputMoney').value=sliderMoney.value});
-sliderTime.addEventListener('change', ()=>{document.querySelector('.textInputTime').value=sliderTime.value});
 
-function changeOnInitialValueMoney(initValMoney, actualVal) {
-    
+
+window.onload = () => {
+    fecha.innerHTML = '24/05/2022';
+    presupuesto.innerHTML = '1.200.000';
+    tiempo.innerHTML = '2 Meses';
+    proyecto.innerHTML = '00';
+    descripcion.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga impedit accusantium nisi deserunt cupiditate repellendus magnam illo! Ipsa fuga vero deleniti minima. Eos nam quod ea similique eligendi iure delectus.';
+    etapa.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa consectetur iste architecto odit quis voluptas doloremque odio nulla, tenetur totam neque cumque? Obcaecati cumque, at accusantium excepturi error magnam beatae.';
+    metodologia.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore provident tempore veritatis quo quaerat, rem nisi quod voluptatem, aliquid id eveniet expedita saepe dolor aperiam odit distinctio cum eligendi impedit.';
+    herramientas.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem optio ad sit voluptatibus temporibus. Placeat ducimus molestias veniam similique, totam debitis minus neque asperiores velit alias? Dolore ipsa aliquid ullam?';
 }
